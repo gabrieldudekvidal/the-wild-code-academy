c5defc6801aeb7c4581c3898943b0e18ab3674af
Gabriel Vidal <gabrieldkvl@outlook.com>
2025-02-14 14:01:24 -0800

    content updates

--- index.js
+++ index.js
@@ -18,12 +18,33 @@ app.get("/", (req, res) => {
 });
 
 app.get("/html_css_js", (req, res) => {
-  res.render("html_css_js.ejs");
+  res.render("classes/html_css_js.ejs");
 });
-
 
 app.post("/home", (req, res) => {
   res.render("home.ejs");
+});
+
+/* Classes */
+app.get("/bs5", (req, res) => {
+  res.render("notes/03_bs5.ejs");
+});
+
+app.get("/js", (req, res) => {
+  res.render("notes/04_js.ejs");
+});
+
+/* Assignments */
+app.get("/html_personal_website", (req, res) => {
+  res.render("assignments/html_personal_website.ejs");
+});
+
+app.get("/html_css_news_layout", (req, res) => {
+  res.render("assignments/html_css_news_layout.ejs");
+});
+
+app.get("/js_calculator_app", (req, res) => {
+  res.render("assignments/js_calculator_app.ejs");
 });
 
 app.listen(port, () => {
